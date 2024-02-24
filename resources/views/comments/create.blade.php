6fe152118e43235f6792f2609f5a21f0d034804e
Sergey <chechelsk@gmail.com>
2024-02-24 12:23:29 +0200

    delete some exta code

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Comment</title>
-</head>
-<body>
-    <h2>Add Comment</h2>
+<h2>Add Comment</h2>
 
-    @if ($errors->any())
-        <div>
-            <strong>Validation errors:</strong>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+@if ($errors->any())
+    <div>
+        <strong>Validation errors:</strong>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
-    @include('comments.form')
+@include('comments.form')
 
-</body>
-</html>
